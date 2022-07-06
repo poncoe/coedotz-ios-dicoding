84bb0a3f59331572cd8eae50785c699218f8e32b
poncoe <poncoe4mac@gmail.com>
2022-07-07 01:18:33 +0700

    latihan static table

---
+++ LatihanStaticTable/LatihanStaticTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LatihanStaticTable
+//
+//  Created by Poncoe on 07/07/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
