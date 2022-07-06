67f9fb51cd62da53718e4a408db727e3229971a8
poncoe <poncoe4mac@gmail.com>
2022-07-06 23:34:41 +0700

    latihan iboutlet & ibaction

---
+++ LatihanIBOutletIBAction/LatihanIBOutletIBAction/ProfileViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  LatihanAutoLayout
+//
+//  Created by Poncoe on 15/06/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var btnFollowed: UIButton!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupCircleImageView()
+    }
+    
+    private var isFollow = false
+    
+    @IBAction func setFollowed(_ sender: Any) {
+        isFollow = !isFollow
+        btnFollowed.setTitle(isFollow ? "Following" : "Follow", for: .normal) // Mengganti teks dalam button.
+        print("Anda telah mengikuti Dicoding.")
+    }
+    
+    private func setupCircleImageView(){
+        profileImageView.layer.borderWidth = 1 // Menentukan lebar dari border/garis batas.
+        
+        profileImageView.layer.masksToBounds = false // Menentukan apakah perlu melakukan masking atau tidak.
+        
+        profileImageView.layer.borderColor = UIColor.black.cgColor // Menentukan warna border.
+        
+        profileImageView.layer.cornerRadius = profileImageView.frame.height/2 // Menentukan apakah sudutnya akan membulat (rounded) atau tidak.
+        
+        profileImageView.clipsToBounds = true // Menentukan apakah subview terbatas
+    }
+    
+    
+}
+
