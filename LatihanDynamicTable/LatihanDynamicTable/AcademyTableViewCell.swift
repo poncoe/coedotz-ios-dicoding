b68b86bdd16671e464aadaf3a5663f68b74f3da8
poncoe <poncoe4mac@gmail.com>
2022-07-07 02:31:34 +0700

    latihan dynamic table

---
+++ LatihanDynamicTable/LatihanDynamicTable/AcademyTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  AcademyTableViewCell.swift
+//  LatihanDynamicTable
+//
+//  Created by Poncoe on 07/07/22.
+//
+
+import UIKit
+
+class AcademyTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var academyImageView: UIImageView!
+    
+    @IBOutlet weak var txtLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
