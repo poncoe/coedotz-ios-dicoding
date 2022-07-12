eb6204a94e7806025d1b242929c8427fd49daff3
poncoe <poncoe4mac@gmail.com>
2022-07-12 21:57:10 +0700

    Swift UI

---
+++ LatihanSwiftUI/LatihanSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LatihanSwiftUI
+//
+//  Created by Poncoe on 12/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Coedotz!")
+            .font(.headline)
+            .fontWeight(.bold)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LatihanSwiftUI/LatihanSwiftUI/LatihanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatihanSwiftUIApp.swift
+//  LatihanSwiftUI
+//
+//  Created by Poncoe on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LatihanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
